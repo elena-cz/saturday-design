47b9aded32853bcaa918c063851cc3534a57c706
Elena Czubiak <elenaczubiak@gmail.com>
2019-09-23 13:01:12 -0700

    Add logo

--- src/components/header.js
+++ src/components/header.js
@@ -3,26 +3,12 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './header.module.scss';
 
+import Logo from './logo';
+
 const Header = ({ siteTitle }) => (
-  <header className={styles.header}>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className={`${styles.header} container`}>
+    <div className={styles.logoContainer}>
+      <Logo />
     </div>
   </header>
 );
