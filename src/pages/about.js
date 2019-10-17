56f47c08469c9d34ce53645a5294f1229346083e
Elena Czubiak <elenaczubiak@gmail.com>
2019-10-17 14:45:52 -0700

    Add content and style the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { Link } from 'gatsby';
 
-// import Layout from '../layouts';
+import styles from './about.module.scss';
 import SEO from '../components/seo';
 import Title from '../components/title';
 
-const AboutPage = () => (
-  // <Layout page="about" title="about">
-  <>
-    <SEO title="About" />
-    {/* <Title text="about" htag="h1" center /> */}
-    <Link to="/">Go back to the homepage</Link>
-  </>
-);
+const AboutPage = ({ data }) => {
+  const { markdownRemark } = data; // data.markdownRemark holds our post data
+  const { html } = markdownRemark;
+  return (
+    // <Layout page="about" title="about">
+    <div className={styles.textContainer}>
+      <SEO title="About" />
+      <div
+        className="styled-lists"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query {
+    markdownRemark(frontmatter: { path: { eq: "/about" } }) {
+      html
+    }
+  }
+`;
 
 export default AboutPage;
